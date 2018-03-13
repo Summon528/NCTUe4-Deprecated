df50a34fa052bbb7f59e1b62c7d127cb869d1d7f
CodyTseng <cody880528@hotmail.com>
2018-03-13 23:10:04 +0800

    implement getCourseList & some backend Volley Change

---
+++ app/src/main/java/com/example/codytseng/nctue4/CourseItem.kt
@@ -0,0 +1,10 @@
+package com.example.codytseng.nctue4
+
+/**
+ * Created by CodyTseng on 3/13/2018.
+ */
+class courseItem(courseNo: Int, courseName:String, teacherName: String) {
+    var mTeacherName: String = teacherName
+    var mCourseName: String = courseName
+    var mCourseNo: Int = courseNo
+}
---
+++ app/src/main/java/com/example/codytseng/nctue4/utility/VolleyHandler.kt
@@ -0,0 +1,5 @@
+package com.example.codytseng.nctue4.utility
+
+/**
+ * Created by CodyTseng on 3/13/2018.
+ */
